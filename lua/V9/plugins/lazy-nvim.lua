9110c5683edf4ee1ea9953029395312a3903acd7
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2024-04-07 21:11:35 +0800

    Update lazy-nvim.lua

--- lua/V9/plugins/lazy-nvim.lua
+++ lua/V9/plugins/lazy-nvim.lua
@@ -268,7 +268,7 @@ require("lazy").setup({
     "theHamsta/nvim-dap-virtual-text",
     lazy = true,
     event = { "VeryLazy" },
-    dependencies = { "mfussenegger/nvim-dap" },
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
       require("nvim-dap-virtual-text").setup({})
     end,
@@ -736,48 +736,6 @@ require("lazy").setup({
   {
     "simrat39/rust-tools.nvim",
     lazy = true,
-  },
-
-  {
-    "NTBBloodbath/rest.nvim",
-    lazy = true,
-    ft = "http",
-    init = function()
-      local group = vim.api.nvim_create_augroup("V9_jdtls_rest_http", { clear = true })
-      vim.api.nvim_create_autocmd({ "FileType" }, {
-        group = group,
-        pattern = { "http" },
-        callback = function(o)
-          vim.api.nvim_buf_create_user_command(o.buf, "Http", ":lua require'rest-nvim'.run()", { nargs = 0 })
-          vim.api.nvim_buf_create_user_command(o.buf, "HttpCurl", ":lua require'rest-nvim'.run(true)", { nargs = 0 })
-          vim.api.nvim_buf_create_user_command(o.buf, "HttpLast", ":lua require'rest-nvim'.last()", { nargs = 0 })
-        end,
-      })
-    end,
-    config = function()
-      require("rest-nvim").setup({
-        -- Open request results in a horizontal split
-        result_split_horizontal = false,
-        -- Skip SSL verification, useful for unknown certificates
-        skip_ssl_verification = false,
-        -- Highlight request on run
-        highlight = {
-          enabled = true,
-          timeout = 150,
-        },
-        result = {
-          -- toggle showing URL, HTTP info, headers at top the of result window
-          show_url = true,
-          show_http_info = true,
-          show_headers = true,
-        },
-        -- Jump to request line on run
-        jump_to_request = false,
-        env_file = ".env",
-        custom_dynamic_variables = {},
-        yank_dry_run = true,
-      })
-    end,
   },
 
   -- 选中高亮插件
